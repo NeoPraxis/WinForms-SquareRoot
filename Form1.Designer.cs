3d0dc01c4763effe3c62093cef4c3ddffe940d3b
Kaleb Currier <currierka@husson.edu>
2022-10-02 01:30:30 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SquareRoot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxResult.Font = new System.Drawing.Font("Courier New", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 23;
+            this.listBoxResult.Location = new System.Drawing.Point(45, 176);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(1042, 418);
+            this.listBoxResult.TabIndex = 0;
+            this.listBoxResult.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(931, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Brute Force";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(931, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Heron Method";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(931, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Random";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 669);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxResult);
+            this.Name = "Form1";
+            this.Text = "Square Root Appx";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿/* ********************************************************************
+ * Project:     Square Root Approximation
+ * File:        Form Square Root
+ * Language:    C#
+ * 
+ * Description: This program will approximate the square root of an entered
+ *              number using a couple of different algorithms.
+ * College:     Husson University
+ * Course:      IT 325 - Dr. Gerald Wright
+ * 
+ * Edit History:
+ * Ver  Who Date       Notes
+ * ---- --- ---------- -----------------------------------------------------
+ * 0.1  KMC 09/07/2022 - initial writing
+ *                     - GUI design
+ * 0.2  KMC 09/07/2022 - added brute force algorithm
+ * 0.3  KMC 09/12/2022 - finished brute force alg and made it pretty
+ * 0.4  KMC 09/12/2022 - added Heron Method for square root method
+ * ********************************************************************/
+using System;
+using System.Windows.Forms;
+
+namespace SquareRoot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// When the user clicks the button, read the number from the form
+        /// and calculate the square root.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String userText;
+            double number;
+            double answer;
+
+            // get the number from the form
+            userText = textBox1.Text;
+            number = Convert.ToDouble(userText);
+
+            // calculate the answer
+            answer = SquareRoot_BruteForce(number);
+
+
+            //display to the user
+            MessageBox.Show(answer.ToString());
+
+        }
+
+        /// <summary>
+        /// This routine calculates the square root of the number passed in 
+        /// to the desired precision using a brute force algorithm.
+        /// </summary>
+        /// <param name="number">Find the square root of number</param>
+        /// <param name="precision">The desired precision for the result</param>
+        /// <returns>The square root of the number to the desired precision</returns>
+        private double SquareRoot_BruteForce(double number, double precision = 0.01)
+        {
+            double difference;                   ///difference between out result and current guess
+            double result;                       /// current result
+            double guess;                        /// result squared
+
+            result = 0.0;
+            difference = double.MaxValue;
+
+            //set up the output
+            listBoxResult.Items.Clear();
+            listBoxResult.Items.Add("         Result           Guess      Difference");
+            listBoxResult.Items.Add("=============== =============== ===============");
+
+            while (difference > precision)
+            {
+                /// compute the intermediate results
+
+                guess = result * result;
+                difference = number - guess;
+
+                listBoxResult.Items.Add(String.Format("{0,15:f10} {1,15:f10} {2,15:f10}", result, guess, difference));
+
+                if (difference > precision)
+                {
+                    result = result + precision;
+                }
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// This routine calculates the square root of the number passed in 
+        /// to the desired precision using Heron / Babylonian algorithm.
+        /// 
+        /// The algorithm was taken by (HTTPS)
+        /// </summary>
+        /// <param name="sender">Find the square root of number</param>
+        /// <param name="e">The desired precision for the result</param>
+        /// <returns>The square root of the number to the desired precision</returns>
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double answer = SquareRoot(Convert.ToDouble(textBox1.Text));
+
+                MessageBox.Show(answer.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private double SquareRoot(double number, double precision = 0.01)
+        {
+            double guess;
+            double result = 0.0;
+
+            if (number < 0.9)
+            {
+                throw new ArgumentException("The number must be greater than 0");
+            }
+            else
+            {
+
+                result = 0.0000000001;
+                guess = number / result;
+
+                do
+                {
+                    listBoxResult.Items.Add(String.Format("{0,15:f10} {1,15:f10}", result, guess));
+
+                    result = (result + guess) / 2.0;
+                    guess = number / result;
+
+
+                } while (result - guess > precision);
+
+                return result;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd;             // Declare it
+            rnd = new Random();     // Instaniate it
+
+            // Get a random integer
+            int randomNumber = rnd.Next();
+            MessageBox.Show(randomNumber.ToString());
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
